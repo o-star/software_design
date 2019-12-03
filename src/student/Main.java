e67d1a0ebd87d31bd5607490e0297b6184369ef0
o-star <uh04049@naver.com>
2019-12-03 12:28:01 +0900

    dd

---
+++ src/student/Main.java
@@ -0,0 +1,47 @@
+package student;
+
+import student.Student;
+import carrer.nonSubject.*;
+import Data.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Student user = Student.getInstance();
+        counseling_history h = new counseling_history();
+        h.scan_nonSubjectActivity();
+        System.out.println(h.getCounseling_number());
+
+
+        field_practice b = new field_practice();
+
+        graduation_requirement c = graduation_requirement.getInstance();
+        System.out.println(c.getCounseling());
+
+        System.out.println(b.getField_credit());
+
+
+        nonSubjectActivity n = new nonSubjectActivity();
+
+        Data_nonSubject d = Data_nonSubject.getInstance();
+        data_curriculum p = data_curriculum.getInstance();
+
+        Arrays.toString(p.getCredit());
+        System.out.println(p.getMajor_credit());
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
